90ac486df46cde92adac9fbfe3916c74dee16c5a
chibiri <chiburu555@icloud.com>
2023-02-16 15:49:51 +0900

    chibiri

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,5 @@
 import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import type { AppProps } from 'next/app'//urjiugojwu9riebghfioserhgioerh09gfureh9i
 
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />

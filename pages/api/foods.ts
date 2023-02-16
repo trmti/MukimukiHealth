9c9ebb3406d0df2fb10fddfa8072125eaa8ec1c7
trmti <trmtikko@gmail.com>
2023-02-16 13:14:00 +0900

    test

---
+++ pages/api/foods.ts
@@ -0,0 +1,19 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import type { NextApiRequest, NextApiResponse } from 'next';
+
+const testData = {
+  1: {
+    name: 'ハンバーグ',
+  },
+};
+
+type Data = {
+  name: string;
+};
+
+export default function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<Data>
+) {
+  res.status(200).json({ name: 'John Doe' });
+}

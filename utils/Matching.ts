71148ace61b0550a821a334e98be903fc4d3032b
nishi <nishimerk.5.le.jp@icloud.com>
2023-02-18 13:43:39 +0900

    j

--- utils/Matching.ts
+++ utils/Matching.ts
@@ -1,11 +1,11 @@
-import { foods } from "./types";
+import { food } from "./types";
 import "./testData";
 import { pokucaree, rikishiIdeal } from "./testData";
 
-const Matching = () => {
-  const keys: string[] = Object.keys(pokucaree);
-  const values_1: number[] = Object.values(pokucaree);
-  const values_2: number[] = Object.values(rikishiIdeal);
+const Matching = (human: food, ryouri: food) => {
+  const keys: string[] = Object.keys(ryouri);
+  const values_1: number[] = Object.values(ryouri);
+  const values_2: number[] = Object.values(human);
   var value_3: number[] = [];
   for (let index: number = 0; index < values_1.length; index++) {
     value_3.push(values_1[index] / values_2[index]);

b6df4bd2482cd1de3f272c3b685993dc8b2623f3
nishi <nishimerk.5.le.jp@icloud.com>
2023-02-24 10:03:34 +0900

    logouthoka

--- pages/logout.tsx
+++ pages/logout.tsx
@@ -1,18 +1,21 @@
-import type { NextPage } from 'next';
-import { app } from '../utils/firebase';
-import { getAuth, signOut } from 'firebase/auth';
-import { useRouter } from 'next/router';
+import type { NextPage } from "next";
+import { app } from "../utils/firebase";
+import { getAuth, signOut } from "firebase/auth";
+import { useRouter } from "next/router";
+import styles from "../styles/logout.module.css";
 
 const Logout: NextPage = () => {
   const router = useRouter();
   const auth = getAuth(app);
   const handleLogout = async () => {
     await signOut(auth);
-    await router.push('/login');
+    await router.push("/login");
   };
   return (
     <>
-      <button onClick={handleLogout}>ログアウト</button>
+      <button className={styles.logout_btn} onClick={handleLogout}>
+        ログアウト
+      </button>
     </>
   );
 };
